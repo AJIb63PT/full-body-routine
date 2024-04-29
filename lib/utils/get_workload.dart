1ce0f9f4e6d7026f1eecaf5fe3a900d586671a40
albert <albert.xalimow@gmail.com>
2024-04-29 15:32:37 +0500

    github workflow

--- lib/utils/get_workload.dart
+++ lib/utils/get_workload.dart
@@ -11,13 +11,15 @@ class WorkloadArg {
 }
 
 List<String> getWorkload(WorkloadArg item) {
+  bool isBW = bool.parse(item.isBW);
+
   if (item.isFocusDay) {
     if (item.quinta == 1) return ['6', '2'];
     if (item.quinta == 2) return ['4', '3'];
     if (item.quinta == 3) return ['3', '4'];
     if (item.quinta == 4) return ['2', '6'];
   }
-  if (item.quinta == 5 && bool.parse(item.isBW)) return ['3', '6'];
+  if (item.quinta == 5 && isBW) return ['3', '6'];
 
   return ['3', '12'];
 }
